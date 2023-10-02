199c3b14f433681a292e54b25a3ec305a0c36b68
Zachary Joyce <zacharyjoyce@Zacharys-MacBook-Pro.local>
2023-10-02 16:20:19 -0500

    Main Page

---
+++ src/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './HomePage.css'; // Create a CSS file for styling
+
+const HomePage = () => {
+    return (
+        <div className="centered-container">
+            <button className="button">Look Up Coaching Trees</button>
+            <button className="button">Upload Data/Update Coaching Trees</button>
+        </div>
+    );
+}
+
+export default HomePage;
